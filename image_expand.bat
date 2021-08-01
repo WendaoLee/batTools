c59ad0d78b34ace69baf4bd5c673b04fa6a41a2f
LeeWendao <leewendao@outlook.com>
2021-08-01 17:14:12 +0800

    修复错误
    之前发现通配符用错了233紧急修复一下

--- image_expand.bat
+++ image_expand.bat
@@ -1,6 +1,6 @@
 @echo off
 
-rem Coded By Wendaolee,a dumb script used to expand picture-files' Byte without changing imagine itself.
+rem Coded By Wendaolee,a dumb script used to expand picture-files' Byte without changing image itself.
 rem Do hope it can help someone who in need nervously
 rem Well,i guess such a dumb script no one would like to Crtl+C|Crtl+V and release with its name,would it?
 
@@ -20,9 +20,9 @@ set /a filesize= temp*1048676
 fsutil file createNew sourcedata %filesize%
 
 :do expand
-@for %%I IN (*.jpg) Do COPY %%I /B + sourcedata %binpath%*_new.jpg&COPY *.jpg %sourcepath%*_source.jpg
-@for %%I IN (*.jpeg) Do COPY %%I /B + sourcedata %binpath%*_new.jpeg&COPY *.jpeg %sourcepath%*_source.jpeg
-@for %%I IN (*.png) Do COPY %%I /B + sourcedata %binpath%*_new.png&COPY *.jpeg %sourcepath%*_source.png
+for %%I IN (*.jpg) Do COPY %%I /B + sourcedata %binpath%%%I_new.jpg&COPY %%I %sourcepath%%%I_source.jpg
+for %%I IN (*.jpeg) Do COPY %%I /B + sourcedata %binpath%%%I_new.jpeg&COPY *.jpeg %sourcepath%%%I_source.jpeg
+for %%I IN (*.png) Do COPY %%I /B + sourcedata %binpath%%%I_new.png&COPY *.jpeg %sourcepath%%%I_source.png
 
 :end
 cls
